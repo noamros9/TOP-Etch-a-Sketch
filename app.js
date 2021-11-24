43db5b5b716d86529bdc0f9b7b748b7868698106
noamros9 <noamros9@gmail.com>
2021-11-24 18:35:18 +0200

    basic functionality. Still need to figure drag vs clicked

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+let mouseDown = 0;
+
+document.body.ondrag = function(){
+    mouseDown = 1;
+}
+
+document.body.ondragend = function(){
+    mouseDown = 0;
+} 
+
+document.body.onmousedown = function() { 
+    mouseDown = 1;
+}
+
+document.body.onmouseup = function() {
+    mouseDown = 0;
+}
+
+const colorCell = (e) => {
+    setTimeout(() => {
+        if(mouseDown) e.target.classList.add('colored');
+    }, 25);
+}
+
+
 const createGrid = (faceSize) => {
     const grid = document.getElementsByClassName('grid')[0];
     grid.style.gridTemplateRows = `repeat(${faceSize}, 1fr)`;
@@ -6,11 +31,13 @@ const createGrid = (faceSize) => {
     for(let i = 0; i < faceSize**2 ; i++){
         let cell = document.createElement('div');
         cell.classList.add('cell');
+        cell.addEventListener('mouseenter', colorCell);
+
         grid.appendChild(cell); 
     }
 }
 
-let faceSize = 16;
+let faceSize = 40;
 createGrid(faceSize);
 
     
